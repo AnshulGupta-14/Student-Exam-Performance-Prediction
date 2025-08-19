4e83dc0da5bea1644536d195d63da7e7c09e4b61
Anshul Gupta <anshulgupta8854@gmail.com>
2025-08-20 00:35:39 +0530

    Prediction Pipeline

--- Flask/src/components/model_trainer.py
+++ Flask/src/components/model_trainer.py
@@ -68,7 +68,6 @@ class ModelTrainer:
 
             model_report=evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models = models)
 
-            print(model_report)
             best_model_score = max(sorted(model_report.values()))
             best_model_name = list(model_report.keys())[list(model_report.values()).index(best_model_score)]
             best_model = None
