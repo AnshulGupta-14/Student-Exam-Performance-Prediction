e9f0403e467a303df7a4541f6bd5a3a6ff58399c
Anshul Gupta <anshulgupta8854@gmail.com>
2025-08-22 00:30:01 +0530

    code cleaning

--- Flask/src/components/data_ingestion.py
+++ Flask/src/components/data_ingestion.py
@@ -3,10 +3,6 @@ import sys
 
 from src.exception import CustomException
 from src.logger import logging
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
-from src.components.model_trainer import ModelTrainer
-from src.components.model_trainer import ModelTrainerConfig
 
 import pandas as pd
 
